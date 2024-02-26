8cb5237de10c271147faef0d43102036244d13c2
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-02-25 23:51:59 -0500

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -14,3 +14,12 @@ rc-update add docker boot
 
 rc-service sshd start
 rc-update add sshd boot
+
+# Portainer Agent
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:2.19.4
