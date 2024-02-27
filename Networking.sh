7d55b9a134e902b9a959fe4b71f2a0b1192e6f26
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-02-27 15:04:19 -0500

    Update Networking.sh

--- Networking.sh
+++ Networking.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+# Disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+
+# Apply sysctl configuration to disable IPv6 immediately
+sudo sysctl -p
+
 # Flush IPv6 addresses from eth0 interface
 sudo ip -6 addr flush dev eth0
 
 # Flush IPv4 addresses from eth0 interface
 sudo ip addr flush dev eth0
 
-# Assign desired IPv4 address (10.0.0.78) to eth0 interface #### CHANGE IP BELOW
-sudo ip addr add 10.0.0.XX/24 dev eth0
+# Assign desired IPv4 address (10.0.0.78) to eth0 interface
+sudo ip addr add 10.0.0.XX dev eth0  #  <---- Confgiure this 
 
 # Set default gateway via 10.0.0.1
 sudo ip route add default via 10.0.0.1
