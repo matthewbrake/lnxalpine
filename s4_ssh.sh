96762d15b1234883c646c585533e07fd055deb3a
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-03-12 20:44:27 -0400

    Update and rename s4_extras to s4_ssh.sh

--- s4_ssh.sh
+++ s4_ssh.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Portainer Agent
-docker run -d \
-  -p 9001:9001 \
-  --name portainer_agent \
-  --restart=always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
-  portainer/agent:2.19.4
-
 # SSH Change to allow root
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
