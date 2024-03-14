4a259f75dca427e917d98f93db088dee1807dc4f
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-03-14 09:38:39 -0400

    Update s1_repo.sh

--- s1_repo.sh
+++ s1_repo.sh
@@ -4,6 +4,8 @@ alpineversion=`cat /etc/alpine-release | cut -d "." -f 1-2 | awk '{print "v"$1}'
 echo $alpineversion
 
 echo "http://dl-cdn.alpinelinux.org/alpine/$alpineversion/community" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/$alpineversion/main" >> /etc/apk/repositories
+
 apk update
 apk upgrade
 
