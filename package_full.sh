6920631f5c766642348d23d9519640ec80d92fb5
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-03-14 00:04:27 -0400

    Create package_full.sh

---
+++ package_full.sh
@@ -0,0 +1,147 @@
+#!/bin/sh
+
+# Ensure the package database is updated
+sudo apk update
+
+# Upgrade all existing packages to their latest versions
+sudo apk upgrade
+
+# Basic Utilities
+sudo apk add bash zsh fish nano vim neovim emacs curl wget git tmux screen htop atop iotop tree pv jq
+
+# Networking Tools
+sudo apk add net-tools bind-tools iproute2 nmap iftop iperf3 traceroute whois openssh-client openssh-server sshpass wireless-tools wireshark tshark
+
+# Security and Encryption
+sudo apk add fail2ban iptables nftables clamav rkhunter gnupg openssl openvpn wireguard-tools
+
+# System Monitoring and Performance
+sudo apk add sysstat dstat glances logrotate lsof strace perf
+
+# Compression and Archiving
+sudo apk add zip unzip bzip2 gzip tar xz 7zip p7zip lzip lrzip
+
+# Disk Utilities and File Systems
+sudo apk add e2fsprogs xfsprogs dosfstools ntfs-3g lvm2 mdadm smartmontools gparted
+
+# Development Tools and Libraries
+sudo apk add build-base linux-headers gdb valgrind ctags git-lfs
+
+# Programming Languages and Frameworks
+sudo apk add python3 py3-pip ruby nodejs npm go rust cargo php perl
+
+# Web Servers and Reverse Proxies
+sudo apk add nginx apache2 lighttpd tomcat-native varnish squid
+
+# Database Servers
+sudo apk add postgresql mysql mariadb-client sqlite mongodb redis
+
+# Containerization and Virtualization
+sudo apk add docker docker-compose podman kubernetes-client
+
+# File Sharing and Synchronization
+sudo apk add rsync nfs-utils cifs-utils samba-client syncthing
+
+# Text Processing and Editing
+sudo apk add awk sed grep gawk diffutils
+
+# Multimedia Tools
+sudo apk add ffmpeg vlc imagemagick graphicsmagick
+
+# Document Processing
+sudo apk add pandoc texlive
+
+# Email Clients and Utilities
+sudo apk add mutt postfix dovecot
+
+# Programming and Markup Languages
+sudo apk add markdown asciidoc
+
+# Fonts and Theming
+sudo apk add fontconfig noto-fonts ttf-dejavu
+
+# Remote Access and File Transfer
+sudo apk add openssh vsftpd
+
+# Monitoring Dashboards
+sudo apk add grafana prometheus
+
+# Log Management
+sudo apk add logrotate rsyslog
+
+# Backup Solutions
+sudo apk add rsnapshot duplicity
+
+# Custom Shell Prompt
+sudo apk add starship
+
+echo "Installation of packages is complete. Review the list and remove any packages not required."
+
+#!/bin/sh
+
+# Ensure your system is up-to-date
+sudo apk update
+sudo apk upgrade
+
+# Install Basic and Essential Utilities
+sudo apk add bash nano vim curl wget git tmux htop tree file grep sed awk gawk less tar gzip bzip2 xz zip unzip which findutils coreutils binutils util-linux moreutils
+
+# Network Utilities and Tools
+sudo apk add net-tools bind-tools iproute2 nmap iftop iperf3 traceroute whois openssh-client sshpass wireless-tools wpa_supplicant ethtool
+
+# Disk and Filesystem Utilities
+sudo apk add e2fsprogs xfsprogs dosfstools ntfs-3g lvm2 hdparm ncdu
+
+# System Monitoring and Management
+sudo apk add sysstat dstat glances logrotate strace lsof
+
+# Compression and Archive Tools
+sudo apk add p7zip lz4
+
+# Security and Encryption Tools
+sudo apk add fail2ban iptables nftables clamav rkhunter gnupg openssl
+
+# Development Tools and Libraries
+sudo apk add build-base linux-headers gdb valgrind strace cmake autoconf automake libtool m4 gcc g++ make patch
+
+# Common Programming Languages and Runtime
+sudo apk add python3 py3-pip ruby nodejs npm go rust php perl
+
+# Web Servers
+sudo apk add nginx apache2 lighttpd
+
+# Database Servers and Tools
+sudo apk add postgresql mysql mariadb sqlite mongodb redis
+
+# Docker and Containerization
+sudo apk add docker docker-compose
+
+# File Sharing and Transfer Utilities
+sudo apk add rsync nfs-utils cifs-utils samba-client curlftpfs
+
+# Text Processing Tools
+sudo apk add diffutils
+
+# Common Dependencies and Libraries
+sudo apk add libc6-compat glibc libgcc libstdc++ musl libintl libcrypto1.1 libssl1.1 zlib libbz2 expat libffi musl-utils readline ncurses-libs ncurses-terminfo-base ca-certificates
+
+# Multimedia Tools
+sudo apk add ffmpeg vlc imagemagick
+
+# Printing and PDF Utilities
+sudo apk add cups ghostscript poppler-utils
+
+# Email and Communication Tools
+sudo apk add mutt postfix dovecot
+
+# VPN and Network Security
+sudo apk add openvpn wireguard-tools
+
+# Version Control
+sudo apk add git subversion
+
+# Remote Access and Management
+sudo apk add openssh rsync
+
+echo "Installation of packages is complete. Please review any packages you don't need and adjust the script accordingly."
+
